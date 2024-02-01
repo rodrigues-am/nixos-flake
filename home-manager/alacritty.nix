44de8a75c3b186af103b5b937103adc7f5e3e497
André Rodrigues <rodrigues.am@usp.br>
2024-02-01 16:22:47 -0300

    retificação de colorScheme.colors -- .palette

--- home-manager/alacritty.nix
+++ home-manager/alacritty.nix
@@ -43,14 +43,14 @@
         };
 
         normal = {
-          black = "#${config.colorScheme.colors.base00}";
-          red = "#${config.colorScheme.colors.base08}";
-          green = "#${config.colorScheme.colors.base0B}";
-          yellow = "#${config.colorScheme.colors.base0A}";
-          blue = "#${config.colorScheme.colors.base0D}";
-          magenta = "#${config.colorScheme.colors.base0E}";
-          cyan = "#${config.colorScheme.colors.base0C}";
-          white = "#${config.colorScheme.colors.base05}";
+          black = "#${config.colorScheme.palette.base00}";
+          red = "#${config.colorScheme.palette.base08}";
+          green = "#${config.colorScheme.palette.base0B}";
+          yellow = "#${config.colorScheme.palette.base0A}";
+          blue = "#${config.colorScheme.palette.base0D}";
+          magenta = "#${config.colorScheme.palette.base0E}";
+          cyan = "#${config.colorScheme.palette.base0C}";
+          white = "#${config.colorScheme.palette.base05}";
 
         };
       };
