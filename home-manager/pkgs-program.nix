5249a4d09da1650330e04e18161abd210b8c1747
André Rodrigues <rodrigues.am@usp.br>
2024-05-10 10:34:12 -0300

    pacotes Rpackages

--- home-manager/pkgs-program.nix
+++ home-manager/pkgs-program.nix
@@ -3,20 +3,90 @@ with pkgs;
 let
   RStudio-with-my-packages = rstudioWrapper.override {
     packages = with rPackages; [
+
+      # geral
+      lubridate
+      tufte
+      pander
+      purrr
+      tibble
+      #scale
+      jsonlite
+      pdftools
+      pdfsearch
+      descr
+xml2
+      #xmlview
+      knitr
+standardize
+
+
+      # organização e limpeza
       dplyr
       tidyr
-      gsheet
       tidytext
       stringr
+      stringi
+      gsheet
       glue
+      readr
+      gtools
+      #corplot
 
       # tables
       gtable
+      tableone
+      gt
+      gtExtras
+      gtsummary
+      huxtable
+      #kable
+      kableExtra
+      xtable
+
+      # modelo
+      lavaan
+      lavaanPlot
+      mirt
+      psych
+      MASS
+      mice
+      lme4
+      forcats
+      networkD3
+      #difNLP
+      ShinyItemAnalysis
+      CTT
+      lmerTest
+
+      # Graph
+      igraph
+      tidygraph
+      intergraph
+      ggraph
+      ggnetwork
+      widyr
+      GGally
+      ggnetwork
+      netCoin
+
+      # topic NLP
+      NLP
+      topicmodels
+      SnowballC
+      tm
 
       # plot
       ggplot2
       ggthemes
       ggrepel
+      hrbrthemes
+      viridis
+      plotly
+      plotrix
+      visdat
+
+
     ];
   };
 in {
