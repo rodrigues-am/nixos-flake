4359f757b604b2acda3c40492500ff052b758546
André Rodrigues <rodrigues.am@usp.br>
2026-07-25 10:50:31 -0300

    Add alias de cleanup.

--- nixos/server/bash.nix
+++ nixos/server/bash.nix
@@ -2,6 +2,7 @@ _: {
   programs = {
     bash.shellAliases = {
       amr-nixos-hermes-server = "sudo nixos-rebuild switch --flake /home/andre/nixos-flake#hermes-server";
+      amr-nixos-cleanup = "nix-collect-garbage --delete-old && sudo nix-collect-garbage -d && sudo /run/current-system/bin/switch-to-configuration boot";
       rebuild = "sudo nixos-rebuild switch --flake /home/andre/nixos-flake#hermes-server";
       via-off = "sudo /usr/local/bin/aruba-via-stop";
       via-on = "sudo /usr/local/bin/aruba-via-connect";
